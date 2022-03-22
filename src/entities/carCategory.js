680d26aa1b1fdc304d78f18b4eabff1922a44797
LCDS97 <lucas.cds1997@gmail.com>
2022-03-22 00:11:11 -0300

    carCategory constructor

--- src/entities/carCategory.js
+++ src/entities/carCategory.js
@@ -1,7 +1,13 @@
 const Base = require('./base/base.js')
 
 class CarCategory extends Base {
+    constructor({ id, name, carIds, price}){
+        super({ id,name })
 
+        this.cardIds = carIds
+        this.price = price
+
+    }
 }
 
 module.exports = carCategory
